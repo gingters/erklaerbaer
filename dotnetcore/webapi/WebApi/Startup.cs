6f0aea6f1f0c1279e04736a94d1a28f87699f76c
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2018-06-18 20:20:01 +0200

    Version is selected by media type
    Use 'accept: application/json;v=1.0' to select V1 explicitely

--- dotnetcore/webapi/WebApi/Startup.cs
+++ dotnetcore/webapi/WebApi/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -28,6 +29,8 @@ namespace WebApi
             services.AddApiVersioning(options =>
             {
                 options.AssumeDefaultVersionWhenUnspecified = true;
+                options.ApiVersionReader = new MediaTypeApiVersionReader();
+                options.ApiVersionSelector = new CurrentImplementationApiVersionSelector(options);
             });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
