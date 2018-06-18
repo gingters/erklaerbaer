e2bdb64f7da66e72d8eaab5b384d0f8f3328a219
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2018-06-18 20:08:11 +0200

    Added API Versioning

--- dotnetcore/webapi/WebApi/Controllers/ValuesController.cs
+++ dotnetcore/webapi/WebApi/Controllers/ValuesController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers
 {
+    [ApiVersion("1.0")]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
--- dotnetcore/webapi/WebApi/Startup.cs
+++ dotnetcore/webapi/WebApi/Startup.cs
@@ -25,6 +25,10 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApiVersioning(options =>
+            {
+                options.AssumeDefaultVersionWhenUnspecified = true;
+            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
